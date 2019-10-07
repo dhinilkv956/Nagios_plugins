b40772f97bebe47f0343166996f8be5b1f3877f2
dhinilkv956 <54274388+dhinilkv956@users.noreply.github.com>
2019-10-07 11:19:56 +0400

    Update check_response.ps1

--- Windows/check_response.ps1
+++ Windows/check_response.ps1
@@ -1,5 +1,11 @@
 # usage ./check_response.ps1 -w 1000 -url https://google.com
 
+#Locate the below line in nsclient.conf 
+#  [/settings/external scripts/scripts]
+# add the below line 
+# check_response = cmd /c echo scripts\check_response.ps1 $ARG1$; exit $LastExitCode | powershell.exe /noprofile -command -
+
+
 
 Param(
    [Parameter(Position=1)]
